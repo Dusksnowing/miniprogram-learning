069f60841517e79274cc1379536f01f1b5c7e10f
Dusksnow <599286017@qq.com>
2020-06-08 20:54:32 +0800

    生命周期

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
+  //页面初始化时
+  onLaunch() {
+    // 获取用户信息,异步
+    wx.getUserInfo({     
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res)
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    console.log(`init`)
+    setTimeout(()=>{
+      const err = new Error;
+      throw err;
+    }, 2000)
   },
-  globalData: {
-    userInfo: null
+  
+  //界面显示出来的周期函数
+  onShow(){
+    console.log(`show`)
+  },
+
+  //界面隐藏时的周期函数
+  onHide(){
+    console.log(`hide`)
+  },
+
+  //发生错误时的周期函数
+  onError(){
+    console.log(`wrong`)
   }
 })
