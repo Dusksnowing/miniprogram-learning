ac47c745baa9000f82c63c486e8f16eea09bfb90
Dusksnow <599286017@qq.com>
2020-06-09 21:22:09 +0800

    image

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,26 +1,15 @@
 Page({
   data:{
-    
+    imgurl:"",
   },
-
-  //页面加载时
-  onLoad(){
-    console.log(`onload`)
-  },
-  //页面初次渲染时
-  onReady(){
-    console.log(`onready`)
-  },
-  //页面显示时
-  onShow(){
-    console.log(`onshow`)
-  },
-  //页面隐藏
-  onHide(){
-    console.log(`onhide`)
-  },
-  //页面跳转被卸载时
-  onUnload(){
-    console.log(`onunload`)
+  chooseImage(){
+    wx.chooseImage({
+      complete: (res) => {
+        this.setData({
+          imgurl:res.tempFilePaths[0],
+        })
+      },
+    })
   }
+  
 })
